fa3a411f9c9120c2eab168086425c1f15e1d718c
Raihan567 <aburaihansarker567@gmail.com>
2022-07-26 16:07:29 +0600

    create data

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React from "react";
+import Award from "./Award/Award";
+import Feature from "./Feature/Feature";
+import Hero from "./Hero/Hero";
+import Location from "./Location/Location";
+import Price from "./Price/Price";
+import Recent from "./Recent/Recent";
+import Team from "./Team/Team";
 
 const Home = () => {
   return (
     <div>
-      <h2>Home</h2>
+      <h2>Welcome, to Home</h2>
+      <Hero></Hero>
+      <Feature></Feature>
+      <Recent></Recent>
+      <Award></Award>
+      <Location></Location>
+      <Team></Team>
+      <Price></Price>
     </div>
   );
 };
