a3ea62608774c1f711a6b2c2d78ab3221ac91617
Raihan567 <aburaihansarker567@gmail.com>
2022-08-03 02:14:53 +0600

    Done reantup react website

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
-import './Contact.css'
+import React from "react";
+import Back from "../Common/Back";
+import "./Contact.css";
+import img from '../images/pricing.jpg'
 const Contact = () => {
   return (
-    <div>
-      <h2>Contact</h2>
-    </div>
+    <>
+      <section className="contact mb">
+        <Back
+          name="Contact Us"
+          title="Get Helps & Friendly Support"
+          cover={img}
+        />
+        <div className="container">
+          <form className="shadow">
+            <h4>Fill-up The Form</h4> <br />
+            <div>
+              <input type="text" placeholder="Name" />
+              <input type="text" placeholder="Email" />
+            </div>
+            <input type="text" placeholder="Subject" />
+            <textarea cols="30" rows="10"></textarea>
+            <button>Submit Request</button>
+          </form>
+        </div>
+      </section>
+    </>
   );
 };
 
