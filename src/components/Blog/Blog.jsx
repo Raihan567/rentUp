e5c065ad4067b9c0ebf9bcae402187ed2b5499af
Raihan567 <aburaihansarker567@gmail.com>
2022-08-03 02:06:35 +0600

    make full site mobile and tablet responsive

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
-import Back from '../Common/Back';
+import React from "react";
+import Back from "../Common/Back";
+import RecentCard from "../Home/Recent/RecentCard";
+import img from "../images/about.jpg";
+import "../Home/Recent/Recent.css";
 const Blog = () => {
   return (
     <>
-    <section className="blog mb">
-      <Back name="Blog" title= "Now what's going on in the world" />
-    </section>
+      <section className="blog-out mb">
+        <Back name="Blog" title="Blog -Grid Out Blog" cover={img} />
+        <div className="container recent">
+          <RecentCard />
+        </div>
+      </section>
     </>
   );
 };
